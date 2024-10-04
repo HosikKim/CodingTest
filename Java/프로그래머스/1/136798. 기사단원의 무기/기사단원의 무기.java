dd256a4274bf7a9427beb9fad295db2a1c132946
HosikKim <57007654+HosikKim@users.noreply.github.com>
2024-10-04 10:22:44 +0900

    [level 1] Title: 기사단원의 무기, Time: 111.37 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int cnt = 0;
+
+        for(int i=1;i<=number;i++){
+            for(int j=1;j*j<=i;j++){
+                if(j*j == i)cnt++;
+                else if(i%j == 0) cnt+=2;
+            }
+            if(cnt > limit){
+                answer += power;
+            }else{
+                answer += cnt;
+            }
+            cnt = 0;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
