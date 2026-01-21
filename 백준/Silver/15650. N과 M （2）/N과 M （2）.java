0c0ee7954da57999b1e37e1b1f55c7dbf5506c91
김호식 <57007654+HosikKim@users.noreply.github.com>
2026-01-21 12:24:36 +0900

    [Silver III] Title: N과 M (2), Time: 192 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N, M;
+    static int[] S;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        S = new int[M];
+
+        backtracking(0, 1);
+    }
+
+    static void backtracking(int len, int start) {
+        if (len == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(S[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i <= N; i++) {
+            S[len] = i;
+            backtracking(len + 1, i + 1);
+        }
+    }
+}
