42460563abfe23d7c7a07a199a8f4895bfe6c726
김호식 <57007654+HosikKim@users.noreply.github.com>
2026-01-22 15:22:06 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 176 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long result = n;
+
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                result = result - result / i;
+                while (n % i == 0) {
+                    n = n / i;
+                }
+            }
+        }
+        if (n > 1) {
+            result = result - result / n;
+        }
+        System.out.println(result);
+    }
+}
