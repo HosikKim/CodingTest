ba18093439e46bbf78f4e2fe8fa1b88f09d3c216
김호식 <57007654+HosikKim@users.noreply.github.com>
2026-02-19 12:17:48 +0900

    [level 1] Title: 최소직사각형, Time: 1.02 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int temp = 0;
+        int maxWidth = 0;
+        int maxHeight = 0;
+        for(int i=0; i<sizes.length; i++){
+            if(sizes[i][1] > sizes[i][0]){
+                temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            
+            if(sizes[i][0] > maxWidth){
+                maxWidth = sizes[i][0];
+            }                          
+            if(sizes[i][1] > maxHeight){
+                maxHeight = sizes[i][1];
+            }
+        }
+        
+        return maxWidth * maxHeight;
+    }
+}
